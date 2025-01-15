530c6c3b9b87a4817e089d5f681f99a395101888
zgh1234 <68458152+zgh1234@users.noreply.github.com>
2025-01-15 11:49:04 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -17,4 +17,9 @@ sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_genera
 #sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 
 # Modify hostname
-#sed -i 's/OpenWrt/P3TERX-Router/g' package/base-files/files/bin/config_generate
+sed -i 's/OpenWrt/LoveMeOs/g' package/base-files/files/bin/config_generate
+sed -i '750a\<tr><td width="33%"><%:编译作者%></td><td style="color: rgb(135,206,235);font-weight: 1000;">魔法少女莉露露</td></tr>' package/lean/autocore/files/x86/index.htm
+sed -i '751a\<tr><td width="33%"><%:源码%></td><td style="color: red;font-weight: 1000;">基于Lean大佬源码编译</td></tr>' package/lean/autocore/files/x86/index.htm
+sed -i '750a\<tr><td width="33%"><%:编译作者%></td><td style="color: rgb(135,206,235);font-weight: 1000;">魔法少女莉露露</td></tr>' package/lean/autocore/files/arm/index.htm
+sed -i '751a\<tr><td width="33%"><%:源码%></td><td style="color: red;font-weight: 1000;">基于Lean大佬源码编译</td></tr>' package/lean/autocore/files/arm/index.htm
+
